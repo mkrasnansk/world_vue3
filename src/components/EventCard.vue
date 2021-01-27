6d66b4e5a43d9c38fbb055bf5e6f65f207599e79
MKrasnansk <miso.krasnansky@gmail.com>
2021-01-27 18:11:31 +0100

    L6-end

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,31 +1,38 @@
 <template>
-  <div class="event-card">
-    <span> @ {{event.time}} on {{event.date}} </span>
-    <h4>{{event.title}} </h4>
-  </div>
+  <router-link class="event-link" :to="{ name: 'EventDetails', params: { id: event.id } }">
+    <div class="event-card">
+      <span> @{{ event.time }} on {{ event.date }} </span>
+      <h4>{{ event.title }}</h4>
+    </div>
+  </router-link>
 </template>
 
 <script>
 export default {
-  name: "EventCard",
   props: {
-    event: Object
-  },
-};
+    event: {
+      type: Object,
+      required: true
+    }
+  }
+}
 </script>
 
 <style scoped>
-.event-card{
+.event-card {
   padding: 1.2em;
   width: 20em;
   cursor: pointer;
   border: 1px solid #333;
   margin-bottom: 1em;
-  transition: .2s ease;
-
+  transition: 0.2s ease;
 }
-.event-card:hover{
+.event-card:hover {
   transform: scale(1.1);
-  transition: .2s ease;
+  transition: 0.2s ease;
+}
+.event-link{
+  color:rgb(39, 107, 167);
+  text-decoration: none;
 }
 </style>
