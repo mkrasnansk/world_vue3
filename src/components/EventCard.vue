3255b5880d22cff0a1ba9425ddb9ca2b08aa0fd6
MKrasnansk <miso.krasnansky@gmail.com>
2021-01-27 15:42:37 +0100

    L3-end

---
+++ src/components/EventCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="event-card">
+    <span> @ {{event.time}} on {{event.date}} </span>
+    <h4>{{event.title}} </h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+  props: {
+    event: Object
+  },
+};
+</script>
+
+<style scoped>
+.event-card{
+  padding: 1.2em;
+  width: 20em;
+  cursor: pointer;
+  border: 1px solid #333;
+  margin-bottom: 1em;
+  transition: .2s ease;
+
+}
+.event-card:hover{
+  transform: scale(1.1);
+  transition: .2s ease;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Events For Good</h1>
+
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    EventCard
+  },
+  data() {
+    return {
+      events: [{
+        id: 1,
+        category: 'animal welfare',
+        title: 'Cat Adoption Day',
+        description: 'Find your new feline friend at this event,',
+        location: 'New York',
+        date: 'January 28, 2021',
+        time: '12:00',
+        petsAllowed: true,
+        organizer: 'Michal Krasnansky'
+      },
+      {
+         id: 2,
+        category: 'animal welfare',
+        title: 'Cat Adoption Day',
+        description: 'Find your new feline friend at this event,',
+        location: 'New York',
+        date: 'January 28, 2021',
+        time: '12:00',
+        petsAllowed: true,
+        organizer: 'Michal Krasnansky'
+      }
+      
+      ]
+    }
+  },
 };
 </script>
+
+<style scoped>
+.events{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
